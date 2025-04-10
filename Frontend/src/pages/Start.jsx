474c7957ff39ea7b2fcd84b15f2d368c859623b8
Marlapati Nitheesh <marlapati.2023ug1112@iiitranchi.ac.in>
2025-04-10 23:08:58 +0530

    changes done

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -5,7 +5,7 @@ const Start = () => {
   return (
     <div>
       <div className="h-screen bg-cover bg-[url(/Image/uber_bg.jpg)]  bg-no-repeat bg-center pt-8 w-full flex justify-between flex-col">
-        <img className="w-14 ml-8 " src="/Image/uber_log.png" alt="Uber Logo" />
+        <img className="w-14 ml-8 " src="https://pathforward.org/wp-content/uploads/2019/01/Uber_Logo_Black-e1547485455995.jpg" alt="" />
 
         <div className="bg-white pb-7 py-4 px-4">
           <h2 className="text-[30px] font-bold">Get Started with Uber</h2>
@@ -15,5 +15,6 @@ const Start = () => {
     </div>
   );
 };
-
+1
 export default Start;
+ 
