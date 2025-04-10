f546b5612b4edec34e75dd86ed9512f243bc6371
Marlapati Nitheesh <marlapati.2023ug1112@iiitranchi.ac.in>
2025-04-10 23:17:32 +0530

    level

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Start = () => {
   return (
     <div>
-      <div className="h-screen bg-cover bg-[url(/Image/uber_bg.jpg)]  bg-no-repeat bg-center pt-8 w-full flex justify-between flex-col">
+      <div className="h-screen bg-cover bg-[url(./Image/uber_bg.jpg)]  bg-no-repeat bg-center pt-8 w-full flex justify-between flex-col">
         <img className="w-14 ml-8 " src="https://pathforward.org/wp-content/uploads/2019/01/Uber_Logo_Black-e1547485455995.jpg" alt="" />
 
         <div className="bg-white pb-7 py-4 px-4">
